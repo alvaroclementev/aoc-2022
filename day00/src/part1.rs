ec167211e44965b1f7320b1a7fe1f6ae8c6a9260
Alvaro Clemente <clemente.varo@gmail.com>
2022-12-02 20:50:32 +0100

    feat: update the template
    Add better input handling to the template

--- day00/src/part1.rs
+++ day00/src/part1.rs
@@ -1,13 +1,30 @@
-fn main() {
-    let input = parse("input.txt");
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
+
+fn main() -> io::Result<()> {
+    let input = parse("input.txt")?;
     let solution = solve(input);
     println!("{solution}");
+    Ok(())
 }
 
-fn parse(path: &str) {}
+fn parse(path: &str) -> io::Result<Vec<Vec<String>>> {
+    let file = File::open(path)?;
+    let lines = io::BufReader::new(file).lines();
 
-fn solve(input: ()) -> u64 {
-    0
+    let mut parsed = Vec::new();
+    for line in lines.into_iter().flatten() {
+        let words: Vec<String> = line.split_whitespace().map(String::from).collect();
+        parsed.push(words);
+    }
+
+    Ok(parsed)
+}
+
+fn solve(input: Vec<Vec<String>>) -> u64 {
+    1
 }
 
 #[cfg(test)]
@@ -16,8 +33,8 @@ mod tests {
 
     #[test]
     fn test_sample() {
-        let input = parse("sample.txt");
+        let input = parse("sample.txt").unwrap();
         let solution = solve(input);
-        println!("{solution}")
+        assert_eq!(solution, 0)
     }
 }
