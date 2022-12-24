924d42d5d7525fc1ae568f0d4469ac947ee0c123
Alvaro Clemente <clemente.varo@gmail.com>
2022-12-24 11:28:30 +0100

    chore: add activate script

---
+++ activate.sh
@@ -0,0 +1,8 @@
+# Source this, do not run it or the alias won't be maintained
+# e.g:
+#   source activate.sh
+echo 'setting up AoC aliases'
+alias p1='cargo run --bin part1'
+alias p2='cargo run --bin part2'
+alias t1='cargo test --bin part1'
+alias t2='cargo test --bin part2'
